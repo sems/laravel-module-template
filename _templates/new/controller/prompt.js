8be6a70d8e3ccd3d3fb8c204b98df5d4b1247bd3
Sem Spanhaak <me@sems.dev>
2020-08-20 16:04:28 +0200

    Added; - artisan-make-file-location composer package - Hygen make controller

---
+++ _templates/new/controller/prompt.js
@@ -0,0 +1,13 @@
+module.exports = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Type name of controller"
+    },
+    {
+        type: 'input',
+        name: 'module',
+        message: "Type name of module to use",
+        default: 'Core'
+    }
+];
